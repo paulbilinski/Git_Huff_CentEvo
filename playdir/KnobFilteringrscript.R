6e2da858fec5b9cca9e8be5a05b261bf80851e6a
Paul Bilinski <pbilinsk@gmail.com>
2014-05-08 13:54:00 -0700

    All files added, analyses tentatively completed and emailed to group

--- playdir/KnobFilteringrscript.R
+++ playdir/KnobFilteringrscript.R
@@ -1,9 +1,9 @@
 setwd("~/Documents/Projects/Huff_CentromereEvo/Git_Huff_CentEvo/playdir/")
 
-Orig <- read.csv("Original_assemblynames_forsubset_tdact.txt", header=FALSE)
-remain <- read.csv("assemblynamesinDB_knobs_vs_tdacttrf.txt", header=FALSE)
+Orig <- read.csv("Sorghum_assemblynames_forsubset.txt", header=FALSE)
+remain <- read.csv("assemblynamesinDB_knobs_vs_sorghumtrf_nodupl.txt", header=FALSE)
 notknownrepeats <- as.data.frame(setdiff(Orig$V1, remain$V1))
-write.csv(notknownrepeats,file = "nonrepeatsknob_tdact.csv")
+write.csv(notknownrepeats,file = "nonknob_tritur.csv")
 
 Orig <- read.csv("Tander_assemblynames_forsubset.txt", header=FALSE)
 remain <- read.csv("assemblynamesinDB_knobs_vs_tandertrf.txt", header=FALSE)
